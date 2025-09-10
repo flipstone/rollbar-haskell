e322d5992c23220d31ccdcc9e48953fc0fa2846c
David Vollbracht <david@flipstone.com>
2025-09-10 17:45:30 -0500

    Sets up multi-ghc build
    This sets up a multi-version ghc build that runs as part of the build
    script and as a build matrix in CI. I enabled the warnings we use on
    other projects where they didn't cause an error in the strict build.

--- scripts/lib/run-in-container.sh
+++ scripts/lib/run-in-container.sh
@@ -2,15 +2,8 @@ if [ "$IN_DEV_CONTAINER" ]; then
   # Already in container, nothing to do
   :
 else
-  EXTRA_ARGS=()
-
-  if [ "$RUN_AS_SERVICE" ]; then
-    EXTRA_ARGS+=(--service-ports)
-    EXTRA_ARGS+=(--use-aliases)
-  fi
-
   # Script was run from outside the container, re-exec inside the container
   # with the same arguments
   docker compose build
-  exec docker compose run --rm "${EXTRA_ARGS[@]}" dev $0 "$@"
+  exec docker compose run --rm dev $0 "$@"
 fi
